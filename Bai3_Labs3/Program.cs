7e3acf5cab80939c25ea828be12a6dbc7edc0893
baonqpd10172 <162962147+baonqpd10172@users.noreply.github.com>
2024-05-19 22:56:04 +0700

    Add files via upload

---
+++ Bai3_Labs3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+
+    public class Program
+    {
+        static void create(string path)
+    {
+        Directory.CreateDirectory(path);
+
+        string filePath = Path.Combine(path, "data.txt");
+        StreamWriter streamWriter = new StreamWriter(filePath);
+        streamWriter.WriteLine("Mssv: Pd10172");
+        streamWriter.WriteLine("Ho Va Ten: Nguyen Quoc Bao");
+    }
+    static void copy(string sourcePath, string destinationPath)
+    {
+        if(Directory.Exists(destinationPath) == false)
+        {
+            Directory.CreateDirectory(destinationPath);
+
+            string[] fileList = Directory.GetFiles(sourcePath);
+            foreach (string file in fileList)
+            {
+                string fileName = Path.GetFileName(file); 
+                string destFile = Path.Combine(destinationPath, fileName);
+                File.Copy(file, destFile, true);
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        string path1 = "data";
+        string path2 = "data";
+        create(path1);
+        copy(path1, path2);
+    }
+    }
+
---
+++ Labs3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Web;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "data.txt";
+        using (FileStream fs = new FileStream(path, FileMode.Create))
+        {
+            using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+            {
+                sw.WriteLine("username: myUsername");
+                sw.WriteLine("password: myPassword");
+            }
+        }
+        using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+        Console.ReadLine();
+    }
+}
+    
+
